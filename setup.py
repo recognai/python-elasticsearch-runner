843610135a88e11f203b2f29c4c0b31e7cb8fcce
Francisco Aranda <francisco@recogn.ai>
2019-02-25 15:15:48 +0100

    chore: code formatter

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from setuptools import setup
 
 setup(
-    name='elasticsearch-runner',
-    version='0.1',
-    packages=['elasticsearch_runner.resources', 'elasticsearch_runner', 'elasticsearch_runner.test'],
-    url='https://bitbucket.org/comperio/comperio-text-analytics',
-    license='For internal use only.',
-    author='Andre Lynum',
-    author_email='andre.lynum@comperiosearch.com',
-    description='Lightweight runner for transient Elasticsearch instances, f.ex for testing.',
-    install_requires=[
-        'PyYAML',
-        'elasticsearch',
-        'requests',
-        'psutil',
-        'lxml',
-        'plac',
-        'tqdm'
+    name="elasticsearch-runner",
+    version="0.1",
+    packages=[
+        "elasticsearch_runner.resources",
+        "elasticsearch_runner",
+        "elasticsearch_runner.test",
     ],
-    package_data={'elasticsearch_runner': ['resources/*.*']}
+    url="https://bitbucket.org/comperio/comperio-text-analytics",
+    license="For internal use only.",
+    author="Andre Lynum",
+    author_email="andre.lynum@comperiosearch.com",
+    description="Lightweight runner for transient Elasticsearch instances, f.ex for testing.",
+    install_requires=[
+        "PyYAML",
+        "elasticsearch",
+        "requests",
+        "psutil",
+        "lxml",
+        "plac",
+        "tqdm",
+    ],
+    package_data={"elasticsearch_runner": ["resources/*.*"]},
 )
